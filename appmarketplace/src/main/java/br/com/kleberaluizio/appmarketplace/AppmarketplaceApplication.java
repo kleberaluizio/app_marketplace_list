73afed37da0c46be0fbc21169b9912db950b73c7
kleberaluizio <kleberaluizio@gmail.com>
2023-10-14 17:08:19 -0300

    Initializing project

---
+++ appmarketplace/src/main/java/br/com/kleberaluizio/appmarketplace/AppmarketplaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.kleberaluizio.appmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmarketplaceApplication.class, args);
+	}
+
+}
---
+++ appmarketplace/src/test/java/br/com/kleberaluizio/appmarketplace/AppmarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kleberaluizio.appmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
