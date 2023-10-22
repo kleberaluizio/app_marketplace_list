1d653ff406540b592ab28af46fd973ec519bad6e
kleberaluizio <kleberaluizio@gmail.com>
2023-10-22 12:03:09 -0300

    Understanding how angular works

--- frontend-interface/marketplace-list/src/app/app.module.ts
+++ frontend-interface/marketplace-list/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
